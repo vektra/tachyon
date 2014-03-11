2baaa798debc42ceca36245089e3c9c5937aab5b
Evan Phoenix <evan@phx.io>
2014-03-10 22:48:59 -0700

    Add support for running a playbook on a remote host

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package tachyon
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/jessevdk/go-flags"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 type Options struct {
 	Vars       map[string]string `short:"s" long:"set" description:"Set a variable"`
 	ShowOutput bool              `short:"o" long:"output" description:"Show command output"`
-	Host       string            `short:"h" long:"host" description:"Run the playbook on another host"`
+	Host       string            `short:"t" long:"host" description:"Run the playbook on another host"`
+	Release    bool              `short:"r" long:"release" description:"Use a release version"`
+	CleanHost  bool              `long:"clean-host" description:"Clean the host cache before using"`
+	Debug      bool              `short:"d" long:"debug" description:"Show all information about commands"`
 }
 
 func Main(args []string) int {
@@ -17,6 +24,12 @@ func Main(args []string) int {
 	args, err := flags.ParseArgs(&opts, args)
 
 	if err != nil {
+		if serr, ok := err.(*flags.Error); ok {
+			if serr.Type == flags.ErrHelp {
+				return 2
+			}
+		}
+
 		fmt.Printf("Error parsing options: %s", err)
 		return 1
 	}
@@ -58,6 +71,127 @@ func Main(args []string) int {
 	return 0
 }
 
+var cUpdateScript = []byte(`#!/bin/bash
+
+cd .tachyon
+
+if ! test -f tachyon; then
+  curl -O https://s3-us-west-2.amazonaws.com/tachyon.vektra.io/sums
+  if which gpg > /dev/null; then
+    gpg --keyserver keys.gnupg.net --recv-key A408199F &
+    curl -O https://s3-us-west-2.amazonaws.com/tachyon.vektra.io/sums.asc &
+  fi
+
+  curl -O https://s3-us-west-2.amazonaws.com/tachyon.vektra.io/tachyon-linux-amd64
+
+  wait
+
+  if which gpg > /dev/null; then
+    if ! gpg --verify sums.asc; then
+      echo "Signature verification failed! Aborting!"
+      exit 1
+    fi
+  fi
+
+  mv tachyon-linux-amd64 tachyon
+  chmod a+x tachyon
+fi
+`)
+
 func runOnHost(opts *Options, args []string) int {
-	return 1
+	fmt.Printf("=== Executing playbook on %s\n", opts.Host)
+
+	ssh := NewSSH(opts.Host)
+	ssh.Debug = opts.Debug
+
+	defer ssh.Cleanup()
+
+	var bootstrap string
+
+	if opts.CleanHost {
+		bootstrap = "rm -rf .tachyon && mkdir -p .tachyon"
+	} else {
+		bootstrap = "mkdir -p .tachyon"
+	}
+
+	err := ssh.Run(bootstrap)
+	if err != nil {
+		fmt.Printf("Error creating remote .tachyon dir: %s\n", err)
+		return 1
+	}
+
+	if opts.Release {
+		fmt.Printf("=== Updating tachyon release...\n")
+
+		c := ssh.Command("cat > .tachyon/update && chmod a+x .tachyon/update")
+
+		c.Stdout = os.Stdout
+		c.Stdin = bytes.NewReader(cUpdateScript)
+		err = c.Run()
+		if err != nil {
+			fmt.Printf("Error updating, well, the updater: %s\n", err)
+			return 1
+		}
+
+		err = ssh.Run("./.tachyon/update")
+		if err != nil {
+			fmt.Printf("Error running updater: %s\n", err)
+			return 1
+		}
+	} else {
+		err = ssh.CopyToHost("tachyon-linux-amd64", ":.tachyon/tachyon")
+		if err != nil {
+			fmt.Printf("Error copying tachyon to vagrant: %s\n", err)
+			return 1
+		}
+	}
+
+	var src string
+
+	var main string
+
+	fi, err := os.Stat(args[1])
+	if fi.IsDir() {
+		src, err = filepath.Abs(args[1])
+		if err != nil {
+			fmt.Printf("Unable to resolve %s: %s", args[1], err)
+		}
+		main = "site.yml"
+	} else {
+		abs, err := filepath.Abs(args[1])
+		if err != nil {
+			fmt.Printf("Unable to resolve %s: %s", args[1], err)
+			return 1
+		}
+
+		main = filepath.Base(abs)
+		src = filepath.Dir(abs)
+	}
+
+	src += "/"
+
+	fmt.Printf("=== Syncing playbook...\n")
+
+	c := exec.Command("rsync", "-av", "-e", ssh.RsyncCommand(), src, ssh.Host+":.tachyon/playbook")
+
+	if opts.Debug {
+		c.Stdout = os.Stdout
+	}
+
+	err = c.Run()
+
+	if err != nil {
+		fmt.Printf("Error copying playbook to vagrant: %s\n", err)
+	}
+
+	fmt.Printf("=== Running playbook...\n")
+	startCmd := fmt.Sprintf("cd .tachyon && sudo ./tachyon -o playbook/%s", main)
+	err = ssh.RunAndShow(startCmd)
+
+	if err != nil {
+		fmt.Printf("Error running playbook on vagrant: %s\n", err)
+		return 1
+	}
+
+	return 0
 }
---
+++ ssh.go
@@ -0,0 +1,153 @@
+package tachyon
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"os/user"
+	"strings"
+)
+
+type SSH struct {
+	Host   string
+	Config string
+	Debug  bool
+
+	removeConfig bool
+	sshOptions   []string
+}
+
+func (s *SSH) RsyncCommand() string {
+	sshArgs := []string{"ssh"}
+	sshArgs = append(sshArgs, s.sshOptions...)
+
+	if s.Config != "" {
+		sshArgs = append(sshArgs, "-F", s.Config)
+	}
+
+	return strings.Join(sshArgs, " ")
+}
+
+func (s *SSH) SSHCommand(cmd string, args ...string) []string {
+	sshArgs := []string{cmd}
+	sshArgs = append(sshArgs, s.sshOptions...)
+
+	if s.Config != "" {
+		sshArgs = append(sshArgs, "-F", s.Config)
+	}
+
+	sshArgs = append(sshArgs, s.Host)
+
+	return append(sshArgs, args...)
+}
+
+func NewSSH(host string) *SSH {
+	s := &SSH{
+		Host: host,
+	}
+
+	if host == ":vagrant" {
+		s.ImportVagrant()
+	}
+
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	tachDir := u.HomeDir + "/.tachyon"
+
+	if _, err := os.Stat(tachDir); err != nil {
+		err = os.Mkdir(tachDir, 0755)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	s.sshOptions = []string{
+		"-o", "ControlMaster=auto",
+		"-o", "ControlPersist=60s",
+		"-o", "ControlPath=" + tachDir + "/tachyon-cp-ssh-%h-%p-%r",
+	}
+
+	return s
+}
+
+func (s *SSH) Cleanup() {
+	if s.removeConfig {
+		os.Remove(s.Config)
+	}
+}
+
+func (s *SSH) ImportVagrant() bool {
+	s.Host = "default"
+	s.removeConfig = true
+
+	out, err := exec.Command("vagrant", "ssh-config").CombinedOutput()
+	if err != nil {
+		fmt.Printf("Unable to execute 'vagrant ssh-config': %s\n", err)
+		return false
+	}
+
+	f, err := ioutil.TempFile("", "tachyon")
+	if err != nil {
+		fmt.Printf("Unable to make tempfile: %s\n", err)
+		return false
+	}
+
+	_, err = f.Write(out)
+	if err != nil {
+		fmt.Printf("Unable to write to tempfile: %s\n", err)
+		return false
+	}
+
+	f.Close()
+
+	s.Config = f.Name()
+
+	return true
+}
+
+func (s *SSH) Command(args ...string) *exec.Cmd {
+	args = s.SSHCommand("ssh", args...)
+	return exec.Command(args[0], args[1:]...)
+}
+
+func (s *SSH) Run(args ...string) error {
+	c := s.Command(args...)
+
+	if s.Debug {
+		fmt.Printf("Run: %#v\n", c.Args)
+		c.Stdout = os.Stdout
+		c.Stderr = os.Stderr
+	}
+
+	return c.Run()
+}
+
+func (s *SSH) RunAndShow(args ...string) error {
+	c := s.Command(args...)
+
+	if s.Debug {
+		fmt.Printf("Run: %#v\n", c.Args)
+	}
+
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+
+	return c.Run()
+}
+
+func (s *SSH) CopyToHost(src, dest string) error {
+	args := s.SSHCommand("scp", src, s.Host+":"+dest)
+	c := exec.Command(args[0], args[1:]...)
+
+	if s.Debug {
+		fmt.Printf("Run: %#v\n", c.Args)
+		c.Stdout = os.Stdout
+		c.Stderr = os.Stderr
+	}
+
+	return c.Run()
+}
